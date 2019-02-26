d148a1826ab3244705cd2ccf428f40c967c71210
Eyal Benmoshe <eyalbenmoshe@Efrains-iMac.local>
2019-02-26 15:39:28 -0800

    finished desktop homepage coding with video functionality

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+	// main video overlay play
+	$("#video1").click(function(){
+		$("#overlay").fadeIn();
+		$("#overlay-content").html('<script src="https://fast.wistia.com/embed/medias/3g2kcbyz9c.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_3g2kcbyz9c videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/3g2kcbyz9c/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>')
+	});
+
+	$("#video2").click(function(){
+		$("#overlay").fadeIn();
+		$("#overlay-content").html('<script src="https://fast.wistia.com/embed/medias/5wkjbgkp7z.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_5wkjbgkp7z videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/5wkjbgkp7z/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>')
+	});
+
+	// close overlay
+	$("#close-overlay").click(function(){
+		$("#overlay").fadeOut();
+	})
+
+	// shipping address close/open
+	$("#shippingBox").click(function() {
+
+		function ShipSame() {
+			$("#shippingBox").css("background", "#fff");
+			$("#shipping-wrapper").slideDown();
+		};
+
+		function ShipDiff() {
+			$("#shippingBox").css("background", "#2A4494");
+			$("#shipping-wrapper").slideUp();
+		};
+
+		return (this.tog = !this.tog) ? ShipSame() : ShipDiff();
+	});
+
+});
