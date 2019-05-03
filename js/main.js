18e85cf2cdc3e9c214ef74863f63190715596ac3
Eyal Benmoshe <eyalbenmoshe@Eyals-MacBook-Air.local>
2019-05-03 00:18:35 -0700

    end of night Thursday

--- js/main.js
+++ js/main.js
@@ -11,6 +11,11 @@ $(document).ready(function() {
 		$("#overlay-content").html('<script src="https://fast.wistia.com/embed/medias/5wkjbgkp7z.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_5wkjbgkp7z videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/5wkjbgkp7z/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>')
 	});
 
+	$("#video3").click(function(){
+		$("#overlay").fadeIn();
+		$("#overlay-content").html('<script src="https://fast.wistia.com/embed/medias/m8j1ywwj68.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_m8j1ywwj68 videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/m8j1ywwj68/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>')
+	});
+
 	$("#viewCert").click(function(){
 		$("#overlay").fadeIn();
 		$("#overlay-content").html('<iframe src="https://docs.google.com/gview?url=https://www.aquatruwater.com/images/AQ_BROCHURE_PerfDATA_MAIN_2.19_R3.pdf&embedded=true" frameborder="0" id="frame"></iframe>')
